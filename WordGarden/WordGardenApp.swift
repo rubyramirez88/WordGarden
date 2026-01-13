a6065f443f1e80db4f179ebedd7865d7af412166
rubyramirez88 <ruizruby160@gmail.com>
2026-01-13 11:48:15 -0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by RAMIREZ, RUBY on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
